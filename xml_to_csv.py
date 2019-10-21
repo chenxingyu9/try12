b5a648cdbfee5aac55b449fed8681234f3ddda84
chenxingyu9 <34433818+chenxingyu9@users.noreply.github.com>
2019-10-21 19:01:22 +0800

    b

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -53,11 +53,11 @@ def xml_to_csv(path):
 
 def main():
     for folder in ['train','test']:
-        image_path = os.path.join(os.getcwd(), ('/images/' + folder))
+        image_path = os.path.join(os.getcwd(), ('/content/try12/data/images/' + folder))
         xml_df = xml_to_csv(image_path)
         
         # saving dataframe as *.csv in 'images/' folder.
-        xml_df.to_csv(('/images/' + folder + '_labels.csv'), index=None)
+        xml_df.to_csv(('/content/try12/data/images/annotations' + folder + '_labels.csv'), index=None)
         print('Successfully converted xml to csv.')
 
 
